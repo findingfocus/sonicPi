1eae19f2ac7dba3a16f7c113817c96cb3804bf19
Paul Thompson <paul@findingfocus.dev>
2025-01-27 17:07:06 -0700

    mix with headphones on

--- song3.rb
+++ song3.rb
@@ -1,4 +1,4 @@
-use_bpm 120
+use_bpm 90
 
 live_loop :met1 do
   sleep 1
@@ -8,7 +8,15 @@ define :pattern do |pattern|
   return pattern.ring.tick == "x"
 end
 
-#Drums
+# Ambient Wind
+live_loop :wind do
+  use_synth :dark_ambience
+  use_synth_defaults sustain: 15, release: 12, amp: 1.5, attack: 3
+  play [:E1, :E2].choose, pan: rrand(-0.5, 0.5), cutoff: rrand(100, 130)
+  sleep rrand(10, 20)
+end
+
+# Kick
 live_loop :kick, sync: :met1 do
   a = 2
   cu = 100
@@ -16,23 +24,20 @@ live_loop :kick, sync: :met1 do
   sleep 0.25
 end
 
+# HiHats
 live_loop :hihat, sync: :met1 do
-  a = 0.9
+  a = 0.6
   sample :hat_tap, amp: a if pattern "x---x---x---x-x-"
   sleep 0.125
 end
 
+# Clap
 live_loop :snare, sync: :met1 do
   a = 1
-  sample :drum_snare_soft, cutoff: 120, amp: a if pattern "----x-------x---"
+  with_fx :echo, mix: 0.1 do
+    sample :drum_snare_soft, cutoff: 120, amp: a if pattern "----x-------x---"
+  end
   sleep 0.5
-end
-
-live_loop :wind do
-  use_synth :dark_ambience
-  use_synth_defaults sustain: 15, release: 10, amp: 3, attack: 2
-  play [:E1, :E2].choose, pan: rrand(-1, 1)
-  sleep rrand(10, 20)
 end
 
 # Melody
@@ -42,45 +47,44 @@ live_loop :melody, sync: :met1 do
   notes = scale(:E2, :minor_pentatonic, num_octaves: 2)
   
   cu = line(40, 95, steps: 32).tick
-  #cu = 1
+  #cu = 90
   
-  #Rest
   if one_in(4)
     sleep [0.25, 0.5].choose
   else
     note = notes.choose
-    a = 1
-    
+    a = 0.8
     
     with_fx :reverb, room: rrand(0.5, 0.9), mix: 0.6 do
       with_fx :echo, phase: [0.25, 0.5].choose, decay: 2 do
-        with_fx :tanh, krunch: 1 do
+        with_fx :tanh, krunch: 0 do
           play note, release: rrand(0.1, 0.5), cutoff: cu, amp: a
         end
       end
     end
-    
     sleep [0.25, 0.5, 0.75].choose
   end
 end
 
 em_pent = (scale :E1, :minor_pentatonic, num_octaves: 2)
 
-#Pad
+# Pad
 live_loop :ambient_pad do
   use_synth :prophet
-  a = 0.5
-  cu = line(55, 85, steps: 32).tick
-  use_synth_defaults attack: 2, sustain: 8, release: 6, amp: a
-  play em_pent.choose, pan: rrand(-0.5, 0.5), cutoff: 30 #cu
+  a = 1
+  cu = line(55, 75, steps: 32).tick
+  with_fx :reverb, room: 0.8 do
+    use_synth_defaults attack: 2, sustain: 7, release: 10, amp: a
+    play em_pent.choose, pan: rrand(-0.5, 0.5), cutoff: cu
+  end
   sleep rrand(8, 16)
 end
 
-#Bass
+# Bass
 live_loop :bass, sync: :met1 do
   use_synth :fm
-  a = 6
-  notes = scale(:E1, :minor_pentatonic)
-  play notes.tick, release: 0.3, amp: a if pattern "-x------x---x-x-"
+  a = 2
+  notes = scale(:E2, :minor_pentatonic)
+  play notes.tick, release: 0.4, amp: a if pattern "-x------x---x-x-"
   sleep 0.5
 end
