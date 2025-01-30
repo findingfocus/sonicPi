e985aa8106e03f646c2d6ecb9cc29fceebd092e6
Paul Thompson <paul@findingfocus.dev>
2025-01-30 15:00:31 -0700

    restructred directory for ease of use

--- docs/SonicPiMIDISetup.rb
+++ docs/SonicPiMIDISetup.rb
@@ -1,14 +1,14 @@
 live_loop :mid1 do
-  midi_cc 0, (ring, 3, 4, 5, 6, 7, 8, 9).tick
+  midi_cc 0, ring(3, 4, 5, 6, 7, 8, 9).tick
   use_synth :fm
-play :e1
+  play :e1
   sleep 1
-  
+
 end
 
 live_loop :mid2, sync: :mid1 do
   midi_cc 1, val_f: rand
-play :e2
+  play :e2
   sleep 0.25
 end
 
@@ -19,8 +19,8 @@ live_loop :mid3, sync: :mid1 do
 end
 
 live_loop :mid4, sync: :mid1 do
- midi_cc 4, val_f: rand  
+  midi_cc 4, val_f: rand  
   sleep 0.5
-use_synth :saw
- play (octs :B2, 3).tick, release: rrand(0.1, 0.5), amp: 0.5, cutoff: 60
+  use_synth :saw
+  play (octs :B2, 3).tick, release: rrand(0.1, 0.5), amp: 0.5, cutoff: 60
 end
